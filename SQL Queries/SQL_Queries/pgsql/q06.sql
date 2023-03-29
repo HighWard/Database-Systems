d8fa75b647f81c98cc83a9eac3c5ef1082030bf7
HighWard <garychess@hotmail.com>
2023-03-29 18:13:14 -0400

    Delete SQL Queries/SQL_Queries/pgsql directory

--- SQL Queries/SQL_Queries/pgsql/q06.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT W.url, title
-FROM WebPages W
-WHERE W.url NOT IN (SELECT url
-                    FROM Evaluation
-                    WHERE extract(year from evaldate) <= '2006')
-ORDER BY W.url
-;
--- SQL Queries/SQL_Queries/pgsql/q17.sql
+++
@@ -1,5 +0,0 @@
-SELECT AVG(num_links) AS avgnumlinks
-FROM (SELECT COUNT(*) AS num_links
-      FROM Links
-      GROUP BY topicid) AS link_counts
-;
